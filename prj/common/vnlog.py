7d9d7ee6d18c37c264a8702c8a8175c8562d117d
laurentHQ <laurent.hq.nguyen@gmail.com>
2022-09-22 10:48:00 +0800

    Update vnlog.py

--- prj/common/vnlog.py
+++ prj/common/vnlog.py
@@ -1,35 +1,35 @@
 # encoding: UTF-8
 import logging
-import os
+import the
 from vtFunction import *
 
 
 class vnLog(object):
 	def __init__(self, name):
-		# 创建一个logger
-		self.logger = logging.getLogger(name)
-		self.logger.setLevel(logging.DEBUG)
-		# 创建一个handler，用于写入日志文件
+		# Create a logger
+		self. logger = logging. getLogger(name)
+		self. logger. setLevel(logging. DEBUG)
+		# Create a handler to write to the log file
 		path = getRootPath()
-		path = os.path.join(path, 'log', name)
-		fh = logging.FileHandler(path)
-		fh.setLevel(logging.DEBUG)
-		# 再创建一个handler，用于输出到控制台
-		ch = logging.StreamHandler()
-		ch.setLevel(logging.DEBUG)
-		# 定义handler的输出格式
-		formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-		fh.setFormatter(formatter)
-		ch.setFormatter(formatter)
-		# 给logger添加handler
-		self.logger.addHandler(fh)
-		self.logger.addHandler(ch)
+		path = os. path. join(path, 'log', name)
+		fh = logging. FileHandler(path)
+		fh. setLevel(logging. DEBUG)
+		# Create another handler for output to the console
+		ch = logging. StreamHandler()
+		ch. setLevel(logging. DEBUG)
+		# Define the output format of the handler
+		formatter = logging. Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+		fh. setFormatter(formatter)
+		ch. setFormatter(formatter)
+		# Add a handler to the logger
+		self. logger. addHandler(fh)
+		self. logger. addHandler(ch)
 
 	def write(self, log):
-		self.logger.info(log)
+		self. logger. info(log)
 		
 
 if __name__ == '__main__':
 	print getRootPath()
 	vnlog = vnLog('test_log.txt')
-	vnlog.write('hi wu')
+	vnlog. write('hi wu')
