13df8b280da45b4c36e613754f74ff1bda7a60e2
laurentHQ <laurent.hq.nguyen@gmail.com>
2022-09-22 12:25:13 +0800

    Update MMBase.py

--- prj/strategy/MMBase.py
+++ prj/strategy/MMBase.py
@@ -1,131 +1,131 @@
 # encoding: UTF-8
 
 '''
-本文件中包含了CTA模块中用到的一些基础设置、类和常量等。
+This file contains some of the basic settings, classes, constants, etc. used in the CTA module.
 '''
 
 from __future__ import division
 
-# 常量定义
-# CTA引擎中涉及到的交易方向类型
-CTAORDER_BUY = u'买开'
-CTAORDER_SELL = u'卖平'
-CTAORDER_SHORT = u'卖开'
-CTAORDER_COVER = u'买平'
+# Constant definition
+# Types of trade directions involved in the CTA engine
+CTAORDER_BUY = u 'buy'
+CTAORDER_SELL = u 'sell flat'
+CTAORDER_SHORT = u 'sell'
+CTAORDER_COVER = u 'buy flat'
 
-# 本地停止单状态
-STOPORDER_WAITING = u'等待中'
-STOPORDER_CANCELLED = u'已撤销'
-STOPORDER_TRIGGERED = u'已触发'
+# Local stop single state
+STOPORDER_WAITING = u 'pending'
+STOPORDER_CANCELLED = u'Undone'
+STOPORDER_TRIGGERED = u 'fired'
 
-# 本地停止单前缀
+# Local stop single prefix
 STOPORDERPREFIX = 'CtaStopOrder.'
 
-# 数据库名称
+# Database name
 SETTING_DB_NAME = 'VnTrader_Setting_Db'
 TICK_DB_NAME = 'VnTrader_Tick_Db'
 DAILY_DB_NAME = 'VnTrader_Daily_Db'
 MINUTE_DB_NAME = 'VnTrader_1Min_Db'
 
-# 引擎类型，用于区分当前策略的运行环境
-ENGINETYPE_BACKTESTING = 'backtesting'  # 回测
-ENGINETYPE_TRADING = 'trading'          # 实盘
+# Engine type, which is used to distinguish the environment in which the current policy is run
+ENGINETYPE_BACKTESTING = 'backtesting' # backtesting
+ENGINETYPE_TRADING = 'trading' # Real
 
-# CTA引擎中涉及的数据类定义
+# Data class definitions involved in the CTA engine
 from vtConstant import EMPTY_UNICODE, EMPTY_STRING, EMPTY_FLOAT, EMPTY_INT
 
 ########################################################################
 class StopOrder(object):
-    """本地停止单"""
+    """Local stop order"""
 
     #----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""
-        self.vtSymbol = EMPTY_STRING
-        self.orderType = EMPTY_UNICODE
-        self.direction = EMPTY_UNICODE
-        self.offset = EMPTY_UNICODE
-        self.price = EMPTY_FLOAT
-        self.volume = EMPTY_INT
+        self. vtSymbol = EMPTY_STRING
+        self. orderType = EMPTY_UNICODE
+        self. direction = EMPTY_UNICODE
+        self. offset = EMPTY_UNICODE
+        self. price = EMPTY_FLOAT
+        self. volume = EMPTY_INT
         
-        self.strategy = None             # 下停止单的策略对象
-        self.stopOrderID = EMPTY_STRING  # 停止单的本地编号 
-        self.status = EMPTY_STRING       # 停止单状态
+        self. strategy = None # The policy object for placing a stop order
+        self. stopOrderID = EMPTY_STRING# Local number of the stop order 
+        self. status = EMPTY_STRING # Stop order state
 
 
 ########################################################################
 class CtaBarData(object):
-    """K线数据"""
+    """K-line data"""
 
     #----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""
-        self.vtSymbol = EMPTY_STRING        # vt系统代码
-        self.symbol = EMPTY_STRING          # 代码
-        self.exchange = EMPTY_STRING        # 交易所
+        self. vtSymbol = EMPTY_STRING # vt system code
+        self. symbol = EMPTY_STRING # code
+        self.  exchange = EMPTY_STRING # exchange
     
-        self.open = EMPTY_FLOAT             # OHLC
-        self.high = EMPTY_FLOAT
-        self.low = EMPTY_FLOAT
-        self.close = EMPTY_FLOAT
+        self. open = EMPTY_FLOAT # OHLC
+        self. high = EMPTY_FLOAT
+        self. low = EMPTY_FLOAT
+        self. close = EMPTY_FLOAT
         
-        self.date = EMPTY_STRING            # bar开始的时间，日期
-        self.time = EMPTY_STRING            # 时间
-        self.datetime = None                # python的datetime时间对象
+        self.  date = EMPTY_STRING # bar start time, date
+        self.  time = EMPTY_STRING# time
+        self. datetime = None of the datetime objects of the python
         
-        self.volume = EMPTY_INT             # 成交量
-        self.openInterest = EMPTY_INT       # 持仓量
+        self. volume = EMPTY_INT # volume
+        self. openInterest = EMPTY_INT # open interest
 
 
 ########################################################################
 class MMTickData(object):
-    """Tick数据"""
+    "Tick Data"""
 
     #----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""       
-        self.vtSymbol = EMPTY_STRING            # vt系统代码
-        self.symbol = EMPTY_STRING              # 合约代码
-        self.exchange = EMPTY_STRING            # 交易所代码
+        self. vtSymbol = EMPTY_STRING # vt system code
+        self. symbol = EMPTY_STRING # contract code
+        self.  exchange = EMPTY_STRING # exchange code
 
-        # 成交数据
-        self.lastPrice = EMPTY_FLOAT            # 最新成交价
-        self.volume = EMPTY_INT                 # 最新成交量
-        self.openInterest = EMPTY_INT           # 持仓量
+        # Deal data
+        self. lastPrice = EMPTY_FLOAT # Latest Transaction Price
+        self. volume = EMPTY_INT # Latest volume
+        self. openInterest = EMPTY_INT # open interest
         
-        self.upperLimit = EMPTY_FLOAT           # 涨停价
-        self.lowerLimit = EMPTY_FLOAT           # 跌停价
+        self. upperLimit = EMPTY_FLOAT # up and down price
+        self. lowerLimit = EMPTY_FLOAT # Stop Price
         
-        # tick的时间
-        self.date = EMPTY_STRING            # 日期
-        self.time = EMPTY_STRING            # 时间
+        # tick time
+        self.  date = EMPTY_STRING # date
+        self.  time = EMPTY_STRING# time
         # self.lastTradeTime = EMPTY_STRING
 
-        self.datetime = None                # python的datetime时间对象
+        self. datetime = None of the datetime objects of the python
 
-        self.bids = []
-        self.asks = []
-        # 五档行情
-        self.bidPrice1 = EMPTY_FLOAT
-        self.bidPrice2 = EMPTY_FLOAT
-        self.bidPrice3 = EMPTY_FLOAT
-        self.bidPrice4 = EMPTY_FLOAT
-        self.bidPrice5 = EMPTY_FLOAT
+        self. bids = []
+        self. asks = []
+        # Five levels of quotes
+        self. bidPrice1 = EMPTY_FLOAT
+        self. bidPrice2 = EMPTY_FLOAT
+        self. bidPrice3 = EMPTY_FLOAT
+        self. bidPrice4 = EMPTY_FLOAT
+        self. bidPrice5 = EMPTY_FLOAT
         
-        self.askPrice1 = EMPTY_FLOAT
-        self.askPrice2 = EMPTY_FLOAT
-        self.askPrice3 = EMPTY_FLOAT
-        self.askPrice4 = EMPTY_FLOAT
-        self.askPrice5 = EMPTY_FLOAT        
+        self. askPrice1 = EMPTY_FLOAT
+        self. askPrice2 = EMPTY_FLOAT
+        self. askPrice3 = EMPTY_FLOAT
+        self. askPrice4 = EMPTY_FLOAT
+        self. askPrice5 = EMPTY_FLOAT        
         
-        self.bidVolume1 = EMPTY_INT
-        self.bidVolume2 = EMPTY_INT
-        self.bidVolume3 = EMPTY_INT
-        self.bidVolume4 = EMPTY_INT
-        self.bidVolume5 = EMPTY_INT
+        self. bidVolume1 = EMPTY_INT
+        self. bidVolume2 = EMPTY_INT
+        self. bidVolume3 = EMPTY_INT
+        self. bidVolume4 = EMPTY_INT
+        self. bidVolume5 = EMPTY_INT
         
-        self.askVolume1 = EMPTY_INT
-        self.askVolume2 = EMPTY_INT
-        self.askVolume3 = EMPTY_INT
-        self.askVolume4 = EMPTY_INT
-        self.askVolume5 = EMPTY_INT    
+        self. askVolume1 = EMPTY_INT
+        self. askVolume2 = EMPTY_INT
+        self. askVolume3 = EMPTY_INT
+        self. askVolume4 = EMPTY_INT
+        self. askVolume5 = EMPTY_INT    
