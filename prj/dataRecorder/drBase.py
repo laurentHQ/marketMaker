4083d38ec229331e7c6f96b111986995ee85b02e
laurentHQ <laurent.hq.nguyen@gmail.com>
2022-09-22 10:52:33 +0800

    Update drBase.py

--- prj/dataRecorder/drBase.py
+++ prj/dataRecorder/drBase.py
@@ -1,109 +1,109 @@
 # encoding: UTF-8
 
 '''
-本文件中包含的数据格式和CTA模块通用，用户有必要可以自行添加格式。
+The data formats included in this file are common to the CTA module, and it is necessary for users to add their own formats.
 '''
 
 from __future__ import division
 
 
-# 把vn.trader根目录添加到python环境变量中
+# Add the vn.trader root directory to the python environment variable
 import sys
-sys.path.append('..')
+sys. path. append('..')
 
 
-# 数据库名称
+# Database name
 SETTING_DB_NAME = 'VnTrader_Setting_Db'
 TICK_DB_NAME = 'VnTrader_Tick_Db'
 DAILY_DB_NAME = 'VnTrader_Daily_Db'
 MINUTE_DB_NAME = 'VnTrader_1Min_Db'
 
 
-# CTA引擎中涉及的数据类定义
-from common.vtConstant import EMPTY_UNICODE, EMPTY_STRING, EMPTY_FLOAT, EMPTY_INT
+# Data class definitions involved in the CTA engine
+from common. vtConstant import EMPTY_UNICODE, EMPTY_STRING, EMPTY_FLOAT, EMPTY_INT
 
 
 ########################################################################
 class DrBarData(object):
-    """K线数据"""
+    """K-line data"""
 
     #----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""
-        self.vtSymbol = EMPTY_STRING        # vt系统代码
-        self.symbol = EMPTY_STRING          # 代码
-        self.exchange = EMPTY_STRING        # 交易所
+        self. vtSymbol = EMPTY_STRING # vt system code
+        self. symbol = EMPTY_STRING # code
+        self.  exchange = EMPTY_STRING # exchange
     
-        self.open = EMPTY_FLOAT             # OHLC
-        self.high = EMPTY_FLOAT
-        self.low = EMPTY_FLOAT
-        self.close = EMPTY_FLOAT
+        self. open = EMPTY_FLOAT # OHLC
+        self. high = EMPTY_FLOAT
+        self. low = EMPTY_FLOAT
+        self. close = EMPTY_FLOAT
         
-        self.date = EMPTY_STRING            # bar开始的时间，日期
-        self.time = EMPTY_STRING            # 时间
-        self.datetime = None                # python的datetime时间对象
+        self.  date = EMPTY_STRING # bar start time, date
+        self.  time = EMPTY_STRING# time
+        self. datetime = None of the datetime objects of the python
 
-        self.highTime = EMPTY_STRING        # the time happening the highest close during the bar
-        self.lowTime = EMPTY_STRING         # the time happening the lowest close during the bar
+        self. highTime = EMPTY_STRING # the time happening the highest close during the bar
+        self. lowTime = EMPTY_STRING # the time happening the lowest close during the bar
 
-        self.volume = EMPTY_INT             # 成交量
-        self.openInterest = EMPTY_INT       # 持仓量
+        self. volume = EMPTY_INT # volume
+        self. openInterest = EMPTY_INT # open interest
 
-        self.turnover = EMPTY_FLOAT         # 成交额
-        self.closePrice = EMPTY_FLOAT       # today closePrice
+        self. turnover = EMPTY_FLOAT # turnover
+        self. closePrice = EMPTY_FLOAT # today closePrice
 
 ########################################################################
 class DrTickData(object):
-    """Tick数据"""
+    """Tick Data"""
 
     #----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""       
-        self.vtSymbol = EMPTY_STRING            # vt系统代码
-        self.symbol = EMPTY_STRING              # 合约代码
-        self.exchange = EMPTY_STRING            # 交易所代码
+        self. vtSymbol = EMPTY_STRING # vt system code
+        self. symbol = EMPTY_STRING # contract code
+        self.  exchange = EMPTY_STRING # exchange code
 
-        # 成交数据
-        self.lastPrice = EMPTY_FLOAT            # 最新成交价
-        self.volume = EMPTY_INT                 # 最新成交量
-        self.turnover = EMPTY_FLOAT             # 成交额
-        self.openInterest = EMPTY_INT           # 持仓量
+        # Deal data
+        self. lastPrice = EMPTY_FLOAT # Latest Transaction Price
+        self. volume = EMPTY_INT # Latest volume
+        self. turnover = EMPTY_FLOAT # turnover
+        self. openInterest = EMPTY_INT # open interest
 
-        self.openPrice = EMPTY_FLOAT            # 今日开盘价
-        self.closePrice = EMPTY_FLOAT           # today closePrice
-        self.preClosePrice = EMPTY_FLOAT        # 昨收盘价
-        self.preSettlementPrice = EMPTY_FLOAT   # 昨结算价
-        self.preOpenInterest = EMPTY_FLOAT      # 昨持仓量
+        self. openPrice = EMPTY_FLOAT # Today's Open price
+        self. closePrice = EMPTY_FLOAT # today closePrice
+        self. preClosePrice = EMPTY_FLOAT # Last Close
+        self. preSettlementPrice = EMPTY_FLOAT # Yesterday's settlement price
+        self. preOpenInterest = EMPTY_FLOAT # Previous Open Positions
         
-        self.upperLimit = EMPTY_FLOAT           # 涨停价
-        self.lowerLimit = EMPTY_FLOAT           # 跌停价
+        self. upperLimit = EMPTY_FLOAT # up and down price
+        self. lowerLimit = EMPTY_FLOAT # Stop Price
         
-        # tick的时间
-        self.date = EMPTY_STRING            # 日期
-        self.time = EMPTY_STRING            # 时间
-        self.datetime = None                # python的datetime时间对象
+        # tick time
+        self.  date = EMPTY_STRING # date
+        self.  time = EMPTY_STRING# time
+        self. datetime = None of the datetime objects of the python
         
-        # 五档行情
-        self.bidPrice1 = EMPTY_FLOAT
-        self.bidPrice2 = EMPTY_FLOAT
-        self.bidPrice3 = EMPTY_FLOAT
-        self.bidPrice4 = EMPTY_FLOAT
-        self.bidPrice5 = EMPTY_FLOAT
+        # Five levels of quotes
+        self. bidPrice1 = EMPTY_FLOAT
+        self. bidPrice2 = EMPTY_FLOAT
+        self. bidPrice3 = EMPTY_FLOAT
+        self. bidPrice4 = EMPTY_FLOAT
+        self. bidPrice5 = EMPTY_FLOAT
         
-        self.askPrice1 = EMPTY_FLOAT
-        self.askPrice2 = EMPTY_FLOAT
-        self.askPrice3 = EMPTY_FLOAT
-        self.askPrice4 = EMPTY_FLOAT
-        self.askPrice5 = EMPTY_FLOAT        
+        self. askPrice1 = EMPTY_FLOAT
+        self. askPrice2 = EMPTY_FLOAT
+        self. askPrice3 = EMPTY_FLOAT
+        self. askPrice4 = EMPTY_FLOAT
+        self. askPrice5 = EMPTY_FLOAT        
         
-        self.bidVolume1 = EMPTY_INT
-        self.bidVolume2 = EMPTY_INT
-        self.bidVolume3 = EMPTY_INT
-        self.bidVolume4 = EMPTY_INT
-        self.bidVolume5 = EMPTY_INT
+        self. bidVolume1 = EMPTY_INT
+        self. bidVolume2 = EMPTY_INT
+        self. bidVolume3 = EMPTY_INT
+        self. bidVolume4 = EMPTY_INT
+        self. bidVolume5 = EMPTY_INT
         
-        self.askVolume1 = EMPTY_INT
-        self.askVolume2 = EMPTY_INT
-        self.askVolume3 = EMPTY_INT
-        self.askVolume4 = EMPTY_INT
-        self.askVolume5 = EMPTY_INT    
+        self. askVolume1 = EMPTY_INT
+        self. askVolume2 = EMPTY_INT
+        self. askVolume3 = EMPTY_INT
+        self. askVolume4 = EMPTY_INT
+        self. askVolume5 = EMPTY_INT
