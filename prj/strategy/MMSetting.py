25a43373781b5149d44dcab74e7e13404a79ee8b
laurentHQ <laurent.hq.nguyen@gmail.com>
2022-09-22 12:31:33 +0800

    Update MMSetting.py

--- prj/strategy/MMSetting.py
+++ prj/strategy/MMSetting.py
@@ -1,11 +1,11 @@
+
 # encoding: UTF-8
 
 '''
-在本文件中引入所有希望在系统中使用的策略类
-
-这个字典中保存了需要运行的策略的名称和策略类的映射关系，
-用户的策略类写好后，先在该文件中引入，并设置好名称，然后
-在CTA_setting.json中写入具体每个策略对象的类和合约设置。
+Introduce all the policy classes that you want to use in your system in this file
+This dictionary holds the name of the policy to be run and the mapping relationship of the policy class.
+After the user's policy class is written, it is first introduced in the file, and the name is set, and then
+Write the class and contract settings for each policy object in CTA_setting.json.
 '''
 
 from MMstrategy_v3 import MarketBalance
