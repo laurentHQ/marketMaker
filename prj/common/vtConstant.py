5eaa48c3a8988ccb2ee06ea326ac5dee814e7b81
laurentHQ <laurent.hq.nguyen@gmail.com>
2022-09-22 10:49:20 +0800

    Update vtConstant.py

--- prj/common/vtConstant.py
+++ prj/common/vtConstant.py
@@ -1,92 +1,92 @@
 # encoding: UTF-8
 
-# 默认空值
+# Default null value
 EMPTY_STRING = ''
 EMPTY_UNICODE = u''
 EMPTY_INT = 0
 EMPTY_FLOAT = 0.0
 
-# 方向常量
-DIRECTION_NONE = u'无方向'
+# Direction constant
+DIRECTION_NONE = u 'no direction'
 DIRECTION_LONG = u'多'
 DIRECTION_SHORT = u'空'
-DIRECTION_UNKNOWN = u'未知'
+DIRECTION_UNKNOWN = u 'Unknown'
 DIRECTION_NET = u'净'
-DIRECTION_SELL = u'卖出'      # IB接口
+DIRECTION_SELL = u'sell' # IB interface
 
-# 开平常量
-OFFSET_NONE = u'无开平'
-OFFSET_OPEN = u'开仓'
-OFFSET_CLOSE = u'平仓'
-OFFSET_CLOSETODAY = u'平今'
-OFFSET_CLOSEYESTERDAY = u'平昨'
-OFFSET_UNKNOWN = u'未知'
+# Open the usual amount
+OFFSET_NONE = u'No opening'
+OFFSET_OPEN = u 'open position'
+OFFSET_CLOSE = u'Close Position'
+OFFSET_CLOSETODAY = u'pingjin'
+OFFSET_CLOSEYESTERDAY = u'flat yesterday'
+OFFSET_UNKNOWN = u 'unknown'
 
-# 状态常量
-STATUS_NOTTRADED = u'未成交'
-STATUS_PARTTRADED = u'部分成交'
-STATUS_ALLTRADED = u'全部成交'
-STATUS_CANCELLED = u'已撤销'
-STATUS_PENDING = u'排队中'
-STATUS_UNKNOWN = u'未知'
+# Status constant
+STATUS_NOTTRADED = u 'unfilled'
+STATUS_PARTTRADED = u'partial deal'
+STATUS_ALLTRADED = u 'All Deal'
+STATUS_CANCELLED = u 'Undone'
+STATUS_PENDING = u 'In Queue'
+STATUS_UNKNOWN = u 'unknown'
 STATUS_NOSENT = u'sentFail'
 STATUS_PRESUBMITTED = u'pre_submitted'
 STATUS_PRECANCELLED = u'pre_cancelled'
 
-# 合约类型常量
-PRODUCT_EQUITY = u'股票'
-PRODUCT_FUTURES = u'期货'
-PRODUCT_OPTION = u'期权'
-PRODUCT_INDEX = u'指数'
-PRODUCT_COMBINATION = u'组合'
-PRODUCT_FOREX = u'外汇'
-PRODUCT_UNKNOWN = u'未知'
-PRODUCT_SPOT = u'现货'
-PRODUCT_DEFER = u'延期'
+# Contract type constant
+PRODUCT_EQUITY = u 'stock'
+PRODUCT_FUTURES = u'futures'
+PRODUCT_OPTION = u'option'
+PRODUCT_INDEX = u 'exponent'
+PRODUCT_COMBINATION = u 'combined'
+PRODUCT_FOREX = u'Forex'
+PRODUCT_UNKNOWN = u 'unknown'
+PRODUCT_SPOT = u 'Spot'
+PRODUCT_DEFER = u 'deferred'
 PRODUCT_NONE = ''
 
-# 价格类型常量
-PRICETYPE_LIMITPRICE = u'限价'
-PRICETYPE_MARKETPRICE = u'市价'
+# Price type constant
+PRICETYPE_LIMITPRICE = u 'limit'
+PRICETYPE_MARKETPRICE = u'mark'
 PRICETYPE_FAK = u'FAK'
 PRICETYPE_FOK = u'FOK'
 
-# 期权类型
-OPTION_CALL = u'看涨期权'
-OPTION_PUT = u'看跌期权'
+# Option type
+OPTION_CALL = u'Call Options'
+OPTION_PUT = u 'Put Option'
 
-# 交易所类型
-EXCHANGE_SSE = 'SSE'       # 上交所
-EXCHANGE_SZSE = 'SZSE'     # 深交所
-EXCHANGE_CFFEX = 'CFFEX'   # 中金所
-EXCHANGE_SHFE = 'SHFE'     # 上期所
-EXCHANGE_CZCE = 'CZCE'     # 郑商所
-EXCHANGE_DCE = 'DCE'       # 大商所
-EXCHANGE_SGE = 'SGE'       # 上金所
-EXCHANGE_UNKNOWN = 'UNKNOWN'# 未知交易所
-EXCHANGE_NONE = ''          # 空交易所
-EXCHANGE_HKEX = 'HKEX'      # 港交所
+# Exchange type
+EXCHANGE_SSE = 'SSE' # SSE
+EXCHANGE_SZSE = 'SZSE' # SZSE
+EXCHANGE_CFFEX = 'CFFEX' # CICC
+EXCHANGE_SHFE = 'SHFE' # previous period
+EXCHANGE_CZCE = 'CZCE' # Zheng Shang Suo
+EXCHANGE_DCE = 'DCE' # Daishon
+EXCHANGE_SGE = 'SGE' # Shangjinsuo
+EXCHANGE_UNKNOWN = 'UNKNOWN'# unknown exchange
+EXCHANGE_NONE = '' # short exchange
+EXCHANGE_HKEX = 'HKEX' # HKEx
 
-EXCHANGE_SMART = 'SMART'       # IB智能路由（股票、期权）
-EXCHANGE_NYMEX = 'NYMEX'       # IB 期货
-EXCHANGE_GLOBEX = 'GLOBEX'     # CME电子交易平台
-EXCHANGE_IDEALPRO = 'IDEALPRO' # IB外汇ECN
+EXCHANGE_SMART = 'SMART' # IB Smart Routing (Stocks, Options).
+EXCHANGE_NYMEX = 'NYMEX' # IB Futures
+EXCHANGE_GLOBEX = 'GLOBEX' # CME electronic trading platform
+EXCHANGE_IDEALPRO = 'IDEALPRO' # IB Forex ECN
 
-EXCHANGE_CME = 'CME'           # CME交易所
-EXCHANGE_ICE = 'ICE'           # ICE交易所
+EXCHANGE_CME = 'CME' # CME Exchange
+EXCHANGE_ICE = 'ICE' # ICE exchange
 
-EXCHANGE_OANDA = 'OANDA'       # OANDA外汇做市商
-EXCHANGE_OKCOIN = 'OKCOIN'     # OKCOIN比特币交易所
-EXCHANGE_HUOBI = 'HUOBI'       # HUOBI比特币交易所
-EXCHANGE_HUOBIETH = 'HUOBIETH' # HUOBI比特币交易所
-EXCHANGE_BTCC = 'BTCC'         # BTCC比特币交易所
+EXCHANGE_OANDA = 'OANDA' # OANDA Forex Market Maker
+EXCHANGE_OKCOIN = 'OKCOIN' # OKCOIN Bitcoin Exchange
+EXCHANGE_HUOBI = 'HUOBI' # HUOBI Bitcoin Exchange
+EXCHANGE_HUOBIETH = 'HUOBIETH' # HUOBI Bitcoin Exchange
+EXCHANGE_BTCC = 'BTCC' # BTCC Bitcoin Exchange
 EXCHANGE_ZHCOIN = 'ZHCOIN'#'TOKENBANK'
 
-# 货币类型
-CURRENCY_USD = 'USD'            # 美元
-CURRENCY_CNY = 'CNY'            # 人民币
-CURRENCY_UNKNOWN = 'UNKNOWN'    # 未知货币
-CURRENCY_NONE = ''              # 空货币
+# Currency type
+CURRENCY_USD = 'USD' # USD
+CURRENCY_CNY = 'CNY' # RMB
+CURRENCY_UNKNOWN = 'UNKNOWN' # unknown currency
+CURRENCY_NONE = '' # Empty currency
 
 #数字货币代码
 SYMBOL_BTC_CNY = 'BTC_CNY'
