a322d4348b091989ba02ea3570bea0e8b87ccf91
laurentHQ <laurent.hq.nguyen@gmail.com>
2022-09-22 12:33:40 +0800

    Update MMTemplate.py

--- prj/strategy/MMTemplate.py
+++ prj/strategy/MMTemplate.py
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 '''
-本文件包含了CTA引擎中的策略开发用模板，开发策略时需要继承CtaTemplate类。
+This file contains templates for policy development in the CTA engine, and the CtaTemplate class needs to be inherited when developing policies.
 '''
 
 from MMBase import *
@@ -10,34 +10,34 @@ from time import sleep
 
 ########################################################################
 class MMTemplate(object):
-    """CTA策略模板"""
+    """CTA Policy Template"""
     
-    # 策略类的名称和作者
+    # The name and author of the policy class
     className = 'MMTemplate'
     author = EMPTY_UNICODE
     
-    # MongoDB数据库的名称，K线数据库默认为1分钟
+    # The name of the MongoDB database, the candlestick database defaults to 1 minute
     tickDbName = TICK_DB_NAME
     barDbName = MINUTE_DB_NAME
     
-    # 策略的基本参数
-    name = EMPTY_UNICODE           # 策略实例名称
-    vtSymbol = EMPTY_STRING        # 交易的合约vt系统代码    
-    productClass = EMPTY_STRING    # 产品类型（只有IB接口需要）
-    currency = EMPTY_STRING        # 货币（只有IB接口需要）
+    # Basic parameters of the policy
+    name = EMPTY_UNICODE # Policy instance name
+    vtSymbol = EMPTY_STRING # Contract vt system code for trading 
+    productClass = EMPTY_STRING # Product type (only required for IB interface).
+     currency = EMPTY_STRING # currency (required only for IB interface).
     
-    # 策略的基本变量，由引擎管理
-    inited = False                 # 是否进行了初始化
-    trading = False                # 是否启动交易，由引擎管理
-    pos = [0, 0]                   # 持仓情况
+    # The basic variables of the policy, managed by the engine
+    inited = False # Whether initialized or not
+    trading = False # Whether to initiate a transaction is managed by the engine
+    pos = [0, 0] # Open Position
     
-    # 参数列表，保存了参数的名称
+    # Parameter list, the name of the parameter is saved
     paramList = ['name',
                  'className',
                  'author',
                  'vtSymbol']
     
-    # 变量列表，保存了变量的名称
+    # A list of variables that hold the name of the variable
     varList = ['inited',
                'trading',
                'pos']
@@ -45,152 +45,151 @@ class MMTemplate(object):
     #----------------------------------------------------------------------
     def __init__(self, mmEngine, setting):
         """Constructor"""
-        self.mmEngine = mmEngine
+        self. mmEngine = mmEngine
 
-        # 设置策略的参数
+        # Set the parameters of the policy
         if setting:
-            d = self.__dict__
-            for key in self.paramList:
+            d = self. __dict__
+            for key in self. paramList:
                 if key in setting:
                     d[key] = setting[key]
     
     #----------------------------------------------------------------------
     def onInit(self):
-        """初始化策略（必须由用户继承实现）"""
+        """Initialization policy (must be implemented by user inheritance)"""
         raise NotImplementedError
     
     #----------------------------------------------------------------------
     def onStart(self):
-        """启动策略（必须由用户继承实现）"""
+        """Startup policy (must be implemented by user inheritance)"""
         raise NotImplementedError
     
     #----------------------------------------------------------------------
     def onStop(self):
-        """停止策略（必须由用户继承实现）"""
+        """Stop policy (must be implemented by user inheritance)"""
         raise NotImplementedError
 
     #----------------------------------------------------------------------
     def onTick(self, tick):
-        """收到行情TICK推送（必须由用户继承实现）"""
+        """Received quote TICK push (must be implemented by user inheritance)"""
         raise NotImplementedError
 
     #----------------------------------------------------------------------
     def onOrder(self, order):
-        """收到委托变化推送（必须由用户继承实现）"""
+        """Delegate change push received (must be implemented by user inheritance)"""
         raise NotImplementedError
     
     #----------------------------------------------------------------------
     def onTrade(self, trade):
-        """收到成交推送（必须由用户继承实现）"""
+        """Deal push received (must be implemented by user inheritance)"""
         raise NotImplementedError
     
     #----------------------------------------------------------------------
     def onBar(self, bar):
-        """收到Bar推送（必须由用户继承实现）"""
+        """Received Bar push (must be implemented by user inheritance)"""
         raise NotImplementedError
     
     #----------------------------------------------------------------------
     def buy(self, vtSymbol, price, volume, stop=False):
-        """买开"""
+        """Buy it"""
         # print 'buy'
-        return self.sendOrder(vtSymbol, CTAORDER_BUY, price, volume, stop)
+        return self. sendOrder(vtSymbol, CTAORDER_BUY, price, volume, stop)
     
     #----------------------------------------------------------------------
     def sell(self, vtSymbol, price, volume, stop=False):
-        """卖平"""
+        """Sell flat"""
         # print 'sell'
-        return self.sendOrder(vtSymbol, CTAORDER_SELL, price, volume, stop)
+        return self. sendOrder(vtSymbol, CTAORDER_SELL, price, volume, stop)
 
     #----------------------------------------------------------------------
     def short(self, vtSymbol, price, volume, stop=False):
-        """卖开"""
-        return self.sendOrder(vtSymbol, CTAORDER_SHORT, price, volume, stop)
+        """Sell out"""
+        return self. sendOrder(vtSymbol, CTAORDER_SHORT, price, volume, stop)
  
     #----------------------------------------------------------------------
     def cover(self, vtSymbol, price, volume, stop=False):
-        """买平"""
-        return self.sendOrder(vtSymbol, CTAORDER_COVER, price, volume, stop)
+        """Buy Flat"""
+        return self. sendOrder(vtSymbol, CTAORDER_COVER, price, volume, stop)
         
     #----------------------------------------------------------------------
     def sendOrder(self, vtSymbol, orderType, price, volume, stop=False):
-        """发送委托"""
-        if self.trading:
-            # 如果stop为True，则意味着发本地停止单
+        """Send Delegate"""
+        if self. trading:
+            # If stop is True, it means that a local stop order is issued
             if stop:
-                vtOrderID = self.mmEngine.sendStopOrder(vtSymbol, orderType, price, volume, self)
+                vtOrderID = self. mmEngine. sendStopOrder(vtSymbol, orderType, price, volume, self)
             else:
-                vtOrderID = self.mmEngine.sendOrder(vtSymbol, orderType, price, volume, self)
+                vtOrderID = self. mmEngine. sendOrder(vtSymbol, orderType, price, volume, self)
             return vtOrderID
         else:
-            # 交易停止时发单返回空字符串
+            # An empty string is returned when a transaction is stopped
             return ''        
         
     #----------------------------------------------------------------------
     def cancelOrder(self, vtOrderID):
-        """撤单"""
-        # 如果发单号为空字符串，则不进行后续操作
+        """Withdraw"""
+        # If the order number is an empty string, no subsequent action is taken
         if not vtOrderID:
             return
 
         if STOPORDERPREFIX in vtOrderID:
-            self.mmEngine.cancelStopOrder(vtOrderID)
+            self. mmEngine. cancelStopOrder(vtOrderID)
         else:
-            self.mmEngine.cancelOrder(vtOrderID)
+            self. mmEngine. cancelOrder(vtOrderID)
 
     # ----------------------------------------------------------------------
     def updateOrderStrategyDict(self, strategy):
-        self.mmEngine.updateOrderStrategyDict(strategy)
+        self. mmEngine. updateOrderStrategyDict(strategy)
 
     # ----------------------------------------------------------------------
     def getAllWorkingOrders(self, vtSymbol):
         """Query all working orders in target vtSymbol"""
-        if self.trading:
-            return self.mmEngine.getAllWorkingOrders(vtSymbol)
+        if self. trading:
+            return self. mmEngine. getAllWorkingOrders(vtSymbol)
 
     # ----------------------------------------------------------------------
     def findVtSymbolWorkingOrders(self, vtSymbol):
-        if self.trading:
-            return self.mmEngine.findVtSymbolWorkingOrders(vtSymbol)
+        if self. trading:
+            return self. mmEngine. findVtSymbolWorkingOrders(vtSymbol)
 
     # ----------------------------------------------------------------------
     def cancelAll(self, vtSymbol):
         """Cancel All Orders for target vtSymbol"""
-        if self.trading:
-            self.mmEngine.cancelAll(vtSymbol)
+        if self. trading:
+            self. mmEngine. cancelAll(vtSymbol)
 
     #----------------------------------------------------------------------
     def insertTick(self, tick):
-        """向数据库中插入tick数据"""
-        self.mmEngine.insertData(self.tickDbName, self.vtSymbol, tick)
+        """Insert tick data into the database"""
+        self. mmEngine. insertData(self. tickDbName, self. vtSymbol, tick)
     
     #----------------------------------------------------------------------
     def insertBar(self, bar):
-        """向数据库中插入bar数据"""
-        self.mmEngine.insertData(self.barDbName, self.vtSymbol, bar)
+        """Insert bar data into the database """
+        self. mmEngine. insertData(self. barDbName, self. vtSymbol, bar)
         
     #----------------------------------------------------------------------
     def loadTick(self, days):
-        """读取tick数据"""
-        return self.mmEngine.loadTick(self.tickDbName, self.vtSymbol, days)
+        """Read tick data"""
+        return self. mmEngine. loadTick(self. tickDbName, self. vtSymbol, days)
     
     #----------------------------------------------------------------------
     def loadBar(self, days):
-        """读取bar数据"""
-        return self.mmEngine.loadBar(self.barDbName, self.vtSymbol, days)
+        """Read bar data"""
+        return self. mmEngine. loadBar(self. barDbName, self. vtSymbol, days)
     
     #----------------------------------------------------------------------
     def writeCtaLog(self, content):
-        """记录CTA日志"""
-        content = self.name + ':' + content
-        self.mmEngine.writeCtaLog(content)
+        """Record CTA logs"""
+        content = self. name + ':' + content
+        self. mmEngine. writeCtaLog(content)
         
     #----------------------------------------------------------------------
     def putEvent(self):
-        """发出策略状态变化事件"""
-        self.mmEngine.putStrategyEvent(self.name)
+        """Emitting Policy State Change Event"""
+        self. mmEngine. putStrategyEvent(self. name)
         
     #----------------------------------------------------------------------
     def getEngineType(self):
-        """查询当前运行的环境"""
-        return self.mmEngine.engineType
-    
+        """Query currently running environment"""
+        return self. mmEngine. engineType
